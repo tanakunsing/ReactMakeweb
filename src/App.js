686921d774c9fbab086b1dad43374c154c93a8e3
Tanakun Singkhen <64222364+tanakunsing@users.noreply.github.com>
2024-07-24 03:37:39 +0700

    update bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
+        <Container>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
      
-          Learn React sdadasdsadasd
+          Learn React sdadasdsadasdfsdfdsfsdfsdfdsf
+        </Container>
+        
     
       </header>
     </div>
